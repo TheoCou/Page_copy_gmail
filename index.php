4cf5ad26005666871e7151f3e30eb84210351e85
TheoCou <91134816+TheoCou@users.noreply.github.com>
2024-02-01 16:47:51 +0100

    division des fichiers

--- index.php
+++ index.php
@@ -4,35 +4,12 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $email = $_POST["mail"];
 }
-
 ?>
 
 <?php
 include_once "./src/head.inc.php";
+
+include_once "./src/body.inc.php";
 ?>
-
-<body>
-
-    <?php
-    include_once "./src/header.inc.php";
-    ?>
-
-    <?php
-    include_once "./src/scrollbtn.inc.php"
-    ?>
-
-    <main>
-
-        <?php
-        include_once "./src/home.inc.php"
-        ?>
-
-
-        <?php
-        include_once "./src/form.inc.php"
-        ?>
-    
-    </main>
-</body>
 
 </html>
---
+++ src/body.inc.php
@@ -0,0 +1,23 @@
+<body>
+
+    <?php
+    include_once "./src/header.inc.php";
+    ?>
+
+    <?php
+    include_once "./src/scrollbtn.inc.php"
+    ?>
+
+    <main>
+
+        <?php
+        include_once "./src/home.inc.php"
+        ?>
+
+
+        <?php
+        include_once "./src/form.inc.php"
+        ?>
+    
+    </main>
+</body>
